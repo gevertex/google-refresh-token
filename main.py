51228decb28339fdc1a9aaa8c08e6e3a8204a7a5
George Sapp <george.sapp85@gmail.com>
2016-10-30 00:21:26 -0400

    Minor changes to project setup and .gitignore

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.redirect("/static/index.html")
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('Hello, World! 123')
+        # self.response.headers['Content-Type'] = 'text/html'
+        # self.response.write('Hello, World! 123')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
