e3c3402f9252718b3f04a2116b6ebf1e542eac83
George Sapp <george.sapp85@gmail.com>
2016-10-30 00:07:13 -0400

    Fixing some issues and uploading html site

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
+        self.redirect("/static/index.html")
+        self.response.headers['Content-Type'] = 'text/html'
         self.response.write('Hello, World! 123')
 
 app = webapp2.WSGIApplication([
