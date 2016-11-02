1fdf1af7b6b5ca02b60d68588374eaec64f3cdad
George Sapp <george.sapp85@gmail.com>
2016-11-02 19:58:35 -0400

    Fix for 300 error

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import webapp2
 
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        # self.redirect("/static/index.html")
-        self.response.headers['Content-Type'] = 'text/html'
+        self.redirect("/static/index.html")
+        # self.response.headers['Content-Type'] = 'text/html'
 
-        f = open('/static/index.html', 'r')
-        self.response.write(f)
+        # f = open('/static/index.html', 'r')
+        # self.response.write(f)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
